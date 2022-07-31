785f9c69c807522a72627a2bdf1477e229f6e869
DEEPAKINDRESH N <deepakindreshn@gmail.com>
2022-07-31 16:48:42 +0530

    Added Note component

---
+++ src/components/Note.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Note() {
+  return (
+    <div className='note'>
+        <span>Hello Latracal Solutions!!!</span>
+        <div className='note-footer'>
+            <small>13/04/2021</small>
+            <div className='delete-icon'>X</div>
+        </div>
+    </div>
+  )
+}
+
+export default Note
---
+++ src/components/NoteList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Note from './Note'
+function NoteList() {
+  return (
+    <Note/>
+  )
+}
+
+export default NoteList
